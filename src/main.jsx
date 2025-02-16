a360577ba824e3840d0511e9b9f691ecb4339717
SDHINSA <sohanad@icloud.com>
2025-02-16 15:07:21 -0700

    finished page 2

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import MainPage from './MainPage.jsx'
+import Page2 from './Page2.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <MainPage />
+    <Page2 />
   </StrictMode>,
 )
