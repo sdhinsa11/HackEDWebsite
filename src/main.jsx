8033f97bda3e7797818d5f501c9667816110b22b
SDHINSA <sohanad@icloud.com>
2025-02-16 15:41:08 -0700

    Done main Page

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import './index.css'
 import MainPage from './MainPage.jsx'
 import TeamPage from './TeamPage.jsx'
 import Page2 from './Page2.jsx'
+import Page3 from './Page3.jsx'
+import Page4 from './Page4.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <MainPage /> */}
-    <TeamPage />
-    {/* <Page2 /> */}
+    <MainPage />
+    {/* <TeamPage /> */}
+    <Page2 />
+    <Page3/>
+    <Page4/>
   </StrictMode>,
 )
