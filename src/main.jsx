27f1e9b5d2b09cde2dfdbaaabad30f61b0c099c1
Diya Khajuria <diyakhajuria@Diyas-MacBook-Air.local>
2025-02-16 15:06:00 -0700

    team page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import MainPage from './MainPage.jsx'
+import TeamPage from './TeamPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MainPage />
+    <TeamPage />
   </StrictMode>,
 )
