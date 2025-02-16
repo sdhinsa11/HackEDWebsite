97e82b43b231d62abfba74e78befccaff6d67635
SDHINSA <sohanad@icloud.com>
2025-02-16 13:54:25 -0700

    Created header

---
+++ src/Page.jsx
@@ -0,0 +1,19 @@
+import './Page.css'
+import './index.css'
+
+function Page() {
+
+  return (
+    <>
+      <h1 className='header'>
+          <div>
+              Logo Placement
+          </div>
+        
+        
+      </h1>
+    </>
+  )
+}
+
+export default Page
